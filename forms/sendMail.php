01bbea8f8388c559599f8e4b13d3a6448450efb0
pasinduSudesh <sudeshdilshan.17@cse.mrt.ac.lk>
2020-12-10 21:33:59 +0530

    fix contact form issues

--- forms/sendMail.php
+++ forms/sendMail.php
@@ -9,6 +9,7 @@ $showingName = 'A2 WEB';
 
 $footerMsg = 'This email was sent by: '.$_SERVER['HTTP_HOST'];
 
+//get the data from form
 $email = $_POST['email'];
 $name = $_POST['name'];
 $contactNumber = $_POST['contactNumber'];
@@ -31,26 +32,22 @@ if(isset($_POST['name'])){
 
     $mail = new PHPMailer;
 
-    // $mail->SMTPDebug = 4;                               // Enable verbose debug output
+    // $mail->SMTPDebug = 4;                               
 
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = $sendingEmail;                 // SMTP username
-    $mail->Password = 'A2@123Leos';                           // SMTP password
-    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 587;                                    // TCP port to connect to
+    $mail->isSMTP();                                    
+    $mail->Host = 'smtp.gmail.com';  
+    $mail->SMTPAuth = true;                              
+    $mail->Username = $sendingEmail;                 
+    $mail->Password = 'A2@123Leos';                          
+    $mail->SMTPSecure = 'tls';                          
+    $mail->Port = 587;                                    
 
     $mail->setFrom($sendingEmail, $showingName);
-    $mail->addAddress($receivingEmail);     // Add a recipient
-    // $mail->addAddress('ellen@example.com');               // Name is optional
+    $mail->addAddress($receivingEmail);     
+              
     $mail->addReplyTo($sendingEmail);
-    // $mail->addCC('cc@example.com');
-    // $mail->addBCC('bcc@example.com');
-
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-    $mail->isHTML(true);                                  // Set email format to HTML
+   
+    $mail->isHTML(true);                                  
 
     $mail->Subject = $subject;
     $mail->Body    = $body;
@@ -65,9 +62,5 @@ if(isset($_POST['name'])){
 }else{
     echo 'Please fill the form';
 }
-
-
-// mail($to,$subject,$body);
-
 
 ?>
