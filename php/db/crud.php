67e39546410fc40de420ce189e7e6e6f942890fd
ThilinaPrasad <thilinaprasad.15@cse.mrt.ac.lk>
2021-01-29 00:23:35 +0530

    projects save to db initiated

---
+++ php/db/crud.php
@@ -0,0 +1,45 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+include_once 'dbConfig.php';
+
+class Crud extends DbConfig
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getData($query)
+    {
+        $result = $this->connection->query($query);
+        if ($result == false) {
+            echo $this->connection->error;
+            return false;
+        }
+
+        $rows = array();
+        while ($row = $result->fetch_assoc()) {
+            $rows[] = $row;
+        }
+        return $rows;
+    }
+
+    public function execute($query)
+    {
+        $result = $this->connection->query($query);
+        if ($result == false) {
+            echo $this->connection->error;
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public function escape_string($value)
+    {
+        return $this->connection->real_escape_string($value);
+    }
+}
+?>
---
+++ php/db/dbConfig.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thili
+ * Date: 1/10/2019
+ * Time: 11:30 AM
+ */
+
+class DbConfig{
+
+    private $_host='localhost'; //host name
+    private $_username='root'; //username for the database
+    private $_password='';  //password for the database
+    private $_database='a2web'; //database name
+    protected $connection;
+    public function __construct(){
+        if(!isset($this->connection)){
+            $this->connection=new mysqli($this->_host,$this->_username,$this->_password,$this->_database);
+
+            if(!$this->connection){
+                echo "Cannot connect to the database Server";
+                exit;
+            }
+        }
+        return $this->connection;
+    }
+}
+
+
+?>
