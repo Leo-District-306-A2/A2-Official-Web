167b9d6e311c896c1daf371f3a79186697d06e9d
pasinduSudesh <sudeshdilshan.17@cse.mrt.ac.lk>
2021-02-07 00:29:30 +0530

    created add projects

--- php/db/crud.php
+++ php/db/crud.php
@@ -52,7 +52,43 @@ class Crud extends DbConfig
         $publishedDate = $this->escape_string($publishedDate);
         $publishedBy = $this->escape_string($publishedBy);
 
-        $query = "INSERT INTO projects (id, project_name, description, fb_link, image, published_date, published_by) VALUES ('$id', '$projectName', '$description', '$fbLink', '$image', '$publishedDate', '$publishedBy')";
+        $query = "INSERT INTO projects (id, project_name, description, fb_link, image_path, published_date, published_by) VALUES ('$id', '$projectName', '$description', '$fbLink', '$image', '$publishedDate', '$publishedBy')";
+        $result = $this->execute($query);
+        return $result;
+    }
+
+    public function getAllProjects()
+    {
+        $query = "SELECT id, project_name, description, fb_link, image_path FROM projects ORDER BY published_date DESC";
+        $result = $this->getData($query);
+        return $result;
+    }
+
+    public function getProjectById($id)
+    {
+        $query = "SELECT * FROM projects WHERE id = '$id'";
+        $result = $this->getData($query);
+        return $result;
+    }
+
+    public function updateProject($id, $projectName, $description, $fbLink, $image, $publishedDate, $publishedBy)
+    {
+        $id = $this->escape_string($id);
+        $projectName = $this->escape_string($projectName);
+        $description = $this->escape_string($description);
+        $fbLink = $this->escape_string($fbLink);
+        $image = $this->escape_string($image);
+        $publishedDate = $this->escape_string($publishedDate);
+        $publishedBy = $this->escape_string($publishedBy);
+
+        $query = "UPDATE projects SET project_name = '$projectName', description = '$description', fb_link = '$fbLink', image_path = '$image', published_date = '$publishedDate', published_by = '$publishedBy'";
+        $result = $this->execute($query);
+        return $result;
+    }
+
+    public function deleteProject($id)
+    {
+        $query = "DELETE  FROM projects WHERE id = '$id'";
         $result = $this->execute($query);
         return $result;
     }
